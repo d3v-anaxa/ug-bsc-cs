4b7d930fda60df287e233431afbc631b04770982
d3v-anaxa <d3.anaxa@gmail.com>
2023-11-23 23:16:07 +0530

    final dsa-3
    updated question + algo + output

--- assignment/dsa-03.cc
+++ assignment/dsa-03.cc
@@ -1,5 +1,45 @@
-// Implement Linked List using templates. Include functions for insertion, deletion and
-// search of a number, reverse the list.
+/*
+   Implement Linked List using templates. Include functions for insertion, deletion and search of a number, reverse the list.
+   */
+
+/* ALGO
+   1. Creating a Singly Linked List:
+   1.1. Define a class 'Node' with a variable 'data' to store the node's data and a variable 'next' to store the reference to the next node.
+   1.2. Define a class 'LinkedList' with a variable 'head' to store the reference to the first node in the list.
+
+   2. Adding a Node to the Head:
+   2.1. Create a new 'Node' with the given data.
+   2.2. Set the 'next' of the new node to the current 'head' of the list.
+   2.3. Update the 'head' of the list to the new node.
+
+   3. Adding a Node to the Tail:
+   3.1. Create a new 'Node' with the given data.
+   3.2. If the list is empty, set the 'head' of the list to the new node.
+   3.3. Otherwise, traverse the list to find the last node and set its 'next' to the new node.
+
+   4. Removing a Node from the Head:
+   4.1. If the list is not empty, update the 'head' of the list to the 'next' of the current 'head'.
+
+   5. Removing a Node from the Tail:
+   5.1. If the list is not empty, traverse the list to find the second last node.
+   5.2. Set the 'next' of the second last node to null.
+
+   6. Reversing the Linked List:
+   6.1. Initialize three pointers: 'prev' to null, 'current' to the 'head' of the list, and 'next'.
+   6.2. While 'current' is not null:
+   6.2.1. Set 'next' to the 'next' of 'current'.
+   6.2.2. Set the 'next' of 'current' to 'prev'.
+   6.2.3. Move 'prev' and 'current' one step forward.
+   6.3. Set the 'head' of the list to 'prev'.
+
+   7. Searching an Element in the Linked List:
+   7.1. Initialize a variable 'current' to the 'head' of the list.
+   7.2. While 'current' is not null:
+   7.2.1. If the 'data' of 'current' is equal to the element to be searched, return true.
+   7.2.2. Move 'current' one step forward.
+   7.3. If the loop completes without finding the element, return false.
+
+*/
 
 #include <bits/stdc++.h>
 using namespace std;
@@ -226,49 +266,40 @@ int main(void){
     return 0;
 }
 
-/*
+/* OUTPUT
    [LINKED LIST]
    Linked List Operations!
    [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
    Enter choice : 1
-   Enter value : -10
-   List +>> -10
+   Enter value : 10
+   List +>> 10
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 2
+   Enter value : -100
+   List <<+ -100
    Linked List Operations!
    [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
    Enter choice : 1
-   Enter value : -20
-   List +>> -20
+   Enter value : 33
+   List +>> 33
    Linked List Operations!
    [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
    Enter choice : 2
-   Enter value : 20
-   List <<+ 20
+   Enter value : 55
+   List <<+ 55
    Linked List Operations!
    [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
-   Enter choice : 2
-   Enter value : 80
-   List <<+ 80
-   Linked List Operations!
-   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
-   Enter choice : 3
-   Enter value : 80
-   Target found
+   Enter choice : 7
+   >>  33 10 -100 55
    Linked List Operations!
    [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
    Enter choice : 4
    Deleted Node [-<<]
    Linked List Operations!
    [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
-   Enter choice : 7
-   >>  -10 20 80
-   Linked List Operations!
-   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
    Enter choice : 5
    Deleted Node [>>-]
-   Linked List Operations!
-   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
-   Enter choice : 7
-   >>  -10 20
    Linked List Operations!
    [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
    Enter choice : 6
@@ -276,7 +307,55 @@ int main(void){
    Linked List Operations!
    [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
    Enter choice : 7
-   >>  20 -10
+   >>  -100 10
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 10
+   [LIST CLEANED]
+   */
+
+/* OUTPUT
+   [LINKED LIST]
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 1
+   Enter value : 12
+   List +>> 12
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 1
+   Enter value : 15
+   List +>> 15
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 1
+   Enter value : -10
+   List +>> -10
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 7
+   >>  -10 15 12
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 6
+   List reversed!
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 7
+   >>  12 15 -10
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 3
+   Enter value : -10
+   Target found
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 5
+   Deleted Node [>>-]
+   Linked List Operations!
+   [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
+   Enter choice : 7
+   >>  12 15
    Linked List Operations!
    [1: insert to head, 2: insert to tail, 3: search, 4: remove from head, 5: remove from tail, 6: reverse list, 7: display list]
    Enter choice : 10
