01c289b1072d13b002ef6cf684a4490f729b9d8e
d3v-anaxa <d3.anaxa@gmail.com>
2023-11-23 23:26:09 +0530

    final dsa-4
    updated question + algo + output

--- assignment/dsa-04.cc
+++ assignment/dsa-04.cc
@@ -3,6 +3,45 @@
   Include functions for insertion, deletion and search of a number, reverse the list.
 */
 
+/* ALGO
+   1. Creating a Doubly Linked List:
+   1.1. Define a class 'Node' with a variable 'data' to store the node's data, a variable 'next' to store the reference to the next node, and a variable 'prev' to store the reference to the previous node.
+   1.2. Define a class 'LinkedList' with a variable 'head' to store the reference to the first node in the list.
+
+   2. Adding a Node to the Head:
+   2.1. Create a new 'Node' with the given data.
+   2.2. If the list is not empty, set the 'prev' of the current 'head' to the new node.
+   2.3. Set the 'next' of the new node to the current 'head' of the list.
+   2.4. Update the 'head' of the list to the new node.
+
+   3. Adding a Node to the Tail:
+   3.1. Create a new 'Node' with the given data.
+   3.2. If the list is empty, set the 'head' of the list to the new node.
+   3.3. Otherwise, traverse the list to find the last node, set its 'next' to the new node, and the 'prev' of the new node to the last node.
+
+   4. Removing a Node from the Head:
+   4.1. If the list is not empty, update the 'head' of the list to the 'next' of the current 'head' and set the 'prev' of the new 'head' to null.
+
+   5. Removing a Node from the Tail:
+   5.1. If the list is not empty, traverse the list to find the second last node.
+   5.2. Set the 'next' of the second last node to null.
+
+   6. Reversing the Linked List:
+   6.1. Initialize three pointers: 'prev' to null, 'current' to the 'head' of the list, and 'next'.
+   6.2. While 'current' is not null:
+   6.2.1. Set 'next' to the 'next' of 'current'.
+   6.2.2. Set the 'next' of 'current' to 'prev' and the 'prev' of 'current' to 'next'.
+   6.2.3. Move 'prev' and 'current' one step forward.
+   6.3. Set the 'head' of the list to 'prev'.
+
+   7. Searching an Element in the Linked List:
+   7.1. Initialize a variable 'current' to the 'head' of the list.
+   7.2. While 'current' is not null:
+   7.2.1. If the 'data' of 'current' is equal to the element to be searched, return true.
+   7.2.2. Move 'current' one step forward.
+   7.3. If the loop completes without finding the element, return false.
+   */
+
 #include <bits/stdc++.h>
 using namespace std;
 
@@ -179,10 +218,8 @@ class DoublyLinkedList{
                         break;
                 case 7: this->display();
                         break;
-                default:
-                    return false;
-            }
-            return true;
+                default: return false;
+            }   return true;
         }
 
 };
@@ -196,7 +233,44 @@ int main(void){
     return 0;
 }
 
-/*
+/* OUTPUT
+   [DOUBLY LINKED LIST INIT]
+   Doubly linked list operations!
+   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
+   Enter  choice : 1
+   Enter value : 10
+   Doubly linked list operations!
+   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
+   Enter  choice : 2
+   Enter value : 200
+   Doubly linked list operations!
+   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
+   Enter  choice : 7
+   List elements [2] :  10 200
+   Doubly linked list operations!
+   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
+   Enter  choice : 3
+   Enter value : 250
+   Target -> 250 not found!
+   Doubly linked list operations!
+   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
+   Enter  choice : 4
+   Removed [HEAD] : 10
+   Doubly linked list operations!
+   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
+   Enter  choice : 5
+   Removed [TAIL] : 200
+   Doubly linked list operations!
+   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
+   Enter  choice : 7
+   No elements to display!
+   Doubly linked list operations!
+   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
+   Enter  choice : 10
+   [DOUBLY LINKED LIST DESTROYED]
+   */
+
+/* OUTPUT
    [DOUBLY LINKED LIST INIT]
    Doubly linked list operations!
    [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
@@ -205,23 +279,23 @@ int main(void){
    Doubly linked list operations!
    [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
    Enter  choice : 1
-   Enter value : 10
-   Doubly linked list operations!
-   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
-   Enter  choice : 7
-   List elements [2] :  10 10
+   Enter value : 41
    Doubly linked list operations!
    [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
    Enter  choice : 1
-   Enter value : 20
+   Enter value : 16
    Doubly linked list operations!
    [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
    Enter  choice : 2
-   Enter value : 30
+   Enter value : 25
+   Doubly linked list operations!
+   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
+   Enter  choice : 1
+   Enter value : -10
    Doubly linked list operations!
    [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
    Enter  choice : 7
-   List elements [4] :  20 10 10 30
+   List elements [5] :  -10 16 41 10 25
    Doubly linked list operations!
    [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
    Enter  choice : 6
@@ -229,20 +303,7 @@ int main(void){
    Doubly linked list operations!
    [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
    Enter  choice : 7
-   List elements [4] :  30 10 10 20
-   Doubly linked list operations!
-   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
-   Enter  choice : 3
-   Enter value : 5
-   Target -> 5 not found!
-   Doubly linked list operations!
-   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
-   Enter  choice : 5
-   Removed [TAIL] : 20
-   Doubly linked list operations!
-   [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
-   Enter  choice : 7
-   List elements [3] :  30 10 10
+   List elements [5] :  25 10 41 16 -10
    Doubly linked list operations!
    [1. Add to head, 2. Add to tail, 3. Search, 4. Remove from head, 5. Remove from tail, 6. Reverse list, 7. Display]
    Enter  choice : 10
