df20efa869a017c456ccb8d899ea298f949a5432
d3v-anaxa <d3.anaxa@gmail.com>
2024-02-12 00:51:40 +0530

    uni header file

---
+++ design-and-analysis-of-algorithms/sorting_with_comparisons/sort.h
@@ -0,0 +1,13 @@
+#ifndef SORT_T
+#define SORT_T
+#include <stdio.h>
+#include <stdlib.h>
+#include <time.h>
+static int comparisons = 0;
+#define SIZE 64
+#include "heapSort.c"
+#include "insertionSort.c"
+#include "mergeSort.c"
+#include "quickSort.c"
+#endif
+
